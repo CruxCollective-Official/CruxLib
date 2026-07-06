d1ea61e499f4663f15588bae65f8c1093f78aca7
mry <humi19793@gmail.com>
2026-07-06 15:09:37 +0900

    test(item): Item生成テストを追加

---
+++ src/test/kotlin/mmo/item/ItemTest.kt
@@ -0,0 +1,14 @@
+package mmo.item
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ItemTest {
+    private val itemType: ItemType = DummyItem()
+
+    @Test
+    fun `creates item with correct type`() {
+        val item = Item(itemType)
+        assertEquals(itemType, item.type)
+    }
+}
