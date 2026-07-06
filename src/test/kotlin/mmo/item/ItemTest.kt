508f3f654dc6cd019cb729257a1e892c331a96da
mry114 <humi19793@gmail.com>
2026-07-06 21:05:56 +0900

    test(item): アイテム作成時にMetaDataを持たせるテストを追加

--- src/test/kotlin/mmo/item/ItemTest.kt
+++ src/test/kotlin/mmo/item/ItemTest.kt
@@ -12,4 +12,17 @@ class ItemTest {
         val item = Item(itemType)
         assertEquals(itemType, item.type)
     }
+
+    @Test
+    fun `creates item containing metadata`() {
+        val testKey = MetaDataKey<String>()
+
+        val item = Item(itemType)
+        val meta = MetaData()
+
+        meta[testKey] = "value"
+        item[metaData] = meta
+
+        assertEquals("value", item[metaData][testKey])
+    }
 }
