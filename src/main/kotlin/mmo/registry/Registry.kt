6f7bc6da4d3521c22bcb15199c7cf2351e45e77a
mry <humi19793@gmail.com>
2026-07-08 14:10:53 +0900

    refactor(registry): RegistryBuilderのファイルが見づらいため分割

--- src/main/kotlin/mmo/registry/Registry.kt
+++ src/main/kotlin/mmo/registry/Registry.kt
@@ -15,30 +15,3 @@ data class Registry<K, V>(
 interface ImmutableRegistry<K, V> {
     fun get(key: K): V
 }
-
-class RegistryBuilder {
-    private val registryElementMap: MutableMap<RegistryTypeKey<*, *>, MutableList<BuilderElementRegistry<*, *>>> = mutableMapOf()
-
-    @Suppress("UNCHECKED_CAST")
-    fun <K, V> build(key: RegistryTypeKey<K, V>): Registry<K, V> {
-        val registry: Registry<K, V> = Registry()
-        for (builderRegistry in registryElementMap[key] ?: mutableListOf()) {
-            for ((k, v) in builderRegistry.registryMap) {
-                registry.put(k as K, v as V)
-            }
-        }
-        return registry
-    }
-
-    fun <K, V> add(key: RegistryTypeKey<K, V>, buildRegistry: BuilderElementRegistry<K, V>) {
-        registryElementMap.getOrPut(key) { mutableListOf() }.add(buildRegistry)
-    }
-}
-
-data class BuilderElementRegistry<K, V>(
-    val registryMap: MutableMap<K, V> = mutableMapOf()
-) {
-    fun register(key: K, element: V) {
-        registryMap[key] = element
-    }
-}
