c7d7d56414cbdbfb5c9a4c4b1db355a0e7efd27a
mry <humi19793@gmail.com>
2026-07-09 13:11:36 +0900

    v0.3.0-betaのpublishを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "2.4.0"
+    `java-library`
+    `maven-publish`
 }
 
 group = "org.crux"
-version = "0.2.0-SNAPSHOT"
+version = "0.3.0-SNAPSHOT"
 
 repositories {
 
@@ -18,6 +20,35 @@ kotlin {
     jvmToolchain(21)
 }
 
+java {
+    withSourcesJar()
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("gpr") {
+            artifactId = "crux-lib"
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/CruxCollective-Official/CruxLib")
+
+            credentials {
+                username = project.findProperty("GITHUB_ACTOR")?.toString()
+                    ?: System.getenv("GITHUB_ACTOR")
+
+                password = project.findProperty("GITHUB_TOKEN")?.toString()
+                    ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
